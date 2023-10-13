1541ffc784a246489dd1c9947933d6d184332203
Geovanni Gonzalez <32276348+GeovanniG@users.noreply.github.com>
2023-10-12 18:02:03 -0700

    Add project files.

---
+++ FeatureManagement/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+using FeatureManagement.Shared.Features;
+using FeatureManagement.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement.Mvc;
+
+namespace FeatureManagement.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [FeatureGate(FeatureFlags.FeatureA)]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ FeatureManagement/Shared/Features/FeatureFlags.cs
@@ -0,0 +1,15 @@
+﻿namespace FeatureManagement.Shared.Features;
+
+public class FeatureFlags
+{
+    public const string FeatureA = "FeatureA";
+    public const string FeatureB = "FeatureB";
+    public const string FeatureC = "FeatureC";
+
+    public static IEnumerable<string> Features()
+    {
+        yield return FeatureA;
+        yield return FeatureB;
+        yield return FeatureC;
+    }
+}
