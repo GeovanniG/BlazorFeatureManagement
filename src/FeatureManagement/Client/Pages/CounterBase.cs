15bfb867ea8d4393523a62af0e27e11f41919c47
Geovanni Gonzalez <32276348+GeovanniG@users.noreply.github.com>
2023-10-18 19:45:06 -0700

    Add a basic counter page

---
+++ src/FeatureManagement/Client/Pages/CounterBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FeatureManagement.Client.Pages;
+
+public class CounterBase : ComponentBase
+{
+    public long Count { get; set; }
+
+    public void IncreaseCount()
+    {
+        Count++;
+    }
+
+    public void DecreaseCount()
+    {
+        Count--;
+    }
+}
